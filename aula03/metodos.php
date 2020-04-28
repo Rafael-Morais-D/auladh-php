f203374772565fea96f40f439605ff605b451fff
RafaelMorais <rafaelmorais.design@gmail.com>
2020-04-27 22:21:25 -0300

    Aula de GET e POST

---
+++ aula03/metodos.php
@@ -0,0 +1,110 @@
+<?php
+    $tituloDaPagina = "Métodos | PHP - Aula 02";
+    require_once("./inc/head.php");
+?>
+<body>
+    <?php require_once("./inc/header.php"); ?>
+
+    <main class="container my-5">
+
+        <?php if($_POST && isset($_POST) ): ?>
+        
+            <?php
+                $nome = $_POST["inputNome"];
+                $email = $_POST["inputEmail"];
+                $senha = $_POST["inputSenha"];
+                $endereco = $_POST["inputEndereco1"];
+                $cidade = $_POST["inputCidade"];
+                $estado = $_POST["inputEstado"];
+                $cep = $_POST["inputCep"];
+                $aceitou = $_POST["inputAceite"];
+            ?>
+
+            <article>
+                <h2>Respostas Recebidas por POST:</h2>
+                <table class="table table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">E-mail</th>
+                            <th scope="col">Senha</th>
+                            <th scope="col" colspan="2">Endereço</th>
+                            <th scope="col">Cidade - UF</th>
+                            <th scope="col">CEP</th>
+                            <th scope="col">PDP</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- DESAFIO: INCLUA CADA UM DOS CAMPOS UTILIZANDO O QUE JÁ APRENDEMOS EM PHP -->
+                        <tr>
+                            <th scope="row"><?= $nome; ?></th>
+                            <td><?= $email; ?></td>
+                            <td><?= $senha; ?></td>
+                            <td colspan="2"><?= $endereco; ?></td>
+                            <td><?= $cidade . " - " . $estado; ?></td>
+                            <td><?= $cep; ?></td>
+                            <td><?= $aceitou == "on" ? "aceitou" : "não aceitou"; ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </article>
+
+        <?php elseif ($_GET && isset($_GET)): ?>
+
+            <?php
+                $nome = $_GET["inputNome"];
+                $email = $_GET["inputEmail"];
+                $senha = $_GET["inputSenha"];
+                $endereco = $_GET["inputEndereco1"];
+                $cidade = $_GET["inputCidade"];
+                $estado = $_GET["inputEstado"];
+                $cep = $_GET["inputCep"];
+                $aceitou = $_GET["inputAceite"];
+
+                /* echo "nome - $nome -- email - $email senha - $senha -- endereco - $endereco -- cidade - $cidade -- estado - $estado -- cep - $cep -- aceitou - $aceitou"; */
+            ?>
+
+            <article>
+                <h2>Respostas Recebidas por GET:</h2>
+
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">E-mail</th>
+                            <th scope="col">Senha</th>
+                            <th scope="col" colspan="2">Endereço</th>
+                            <th scope="col">Cidade - UF</th>
+                            <th scope="col">CEP</th>
+                            <th scope="col">PDP</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- DESAFIO: INCLUA CADA UM DOS CAMPOS UTILIZANDO O QUE JÁ APRENDEMOS EM PHP -->
+                        <tr>
+                        <th scope="row"><?= $nome; ?></th>
+                        <td><?= $email; ?></td>
+                        <td><?= $senha; ?></td>
+                        <td colspan="2"><?= $endereco; ?></td>
+                        <td><?= $cidade . " - " . $estado; ?></td>
+                        <td><?= $cep; ?></td>
+                        <td><?= $aceitou == "on" ? "aceitou" : "não aceitou"; ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+            </article>
+
+        <?php else: ?>
+        
+            <article class="alert alert-warning col-6 mx-auto" role="alert">
+                <p><b>Ops... parece que nenhum dado foi recebido!</b></p>
+                <p>Por favor, preencha o formulário <a href="metodo-get.php" title="Acessar o Formulário GET" rel="next">GET</a> ou o formulário <a href="metodo-post.php" title="Acessar o Formulário POST" rel="next">POST</a>.</p>
+                <p>Se você acabou de preencher o formulário, foi direcionado para essa página e está vendo essa mensagem...<br/>deve ter algum erro no seu código! Nesse caso... bora debugar! hehehe</p>
+            </article>
+
+        <?php endif; ?>
+    </main>
+   <?php require_once("./inc/footer.php"); ?>
+</body>
+</html>
--- desafio/desafio.php
+++ desafio/desafio.php
@@ -1,3 +1,10 @@
+<?php
+    $tituloDaPagina = "Métodos | PHP - Aula 02";
+    require_once("./inc/head.php");
+?>
+<body>
+    <?php require_once("./inc/header.php"); ?>
+    <main class="container my-5">
     <h1>DESAFIO PARA A AULA QUE VEM!</h1>
 
     <p>Percorrer o array $listaDeUsuarios (está no arquivo aula01/index.php entre as linhas 140 e 151 no repositório do Victor) e mostrar cada usuário no  HTML, dentro de uma tabela ou lista.</p>
@@ -19,7 +26,47 @@
             ]
         ];</pre>
 
+        <?php
+            $listaDeUsuarios = [
+                "usuario1" => [
+                    "nome" => "Victor",
+                    "email" => "vtorres@digitalhouse.com",
+                    "senha" => "123456"
+                ],
+                "usuario2" => [
+                    "nome" => "Marcelo",
+                    "email" => "mdiament@digitalhouse.com",
+                    "senha" => "789123"
+                ]
+            ];
+        ?>
+
+        <table class="table mt-5">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Senha</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($listaDeUsuarios as $usuario): ?>
+                    <tr>
+                        <td><?= $usuario["nome"]; ?></td>
+                        <td><?= $usuario["email"]; ?></td>
+                        <td><?= $usuario["senha"]; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+    </main>
+   <?php require_once("./inc/footer.php"); ?>
+</body>
+</html>
+
     <?php
+    /*
 
         $listaDeUsuarios = [
             "usuario1" => [
@@ -39,4 +86,6 @@
                 echo "Usuário: " . $usuario . " Categoria: " . $categoria . " Valor: " . $valor . "<br>"; 
             }
         }
+
+        */
     ?>
