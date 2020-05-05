0c62f8f01b98134a60a9c5bfa9725005336a03c6
RafaelMorais <rafaelmorais.design@gmail.com>
2020-05-04 21:09:36 -0300

    Conteúdo da aula 04 de PHP, iniciando estudos sobre JSON

--- aula04/json-decode.php
+++ aula04/json-decode.php
@@ -1,11 +1,25 @@
 <?php
-    $json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+    $stringJson = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
 
+    // transforma a string json em um objeto
+    $objetoStringJson = json_decode($stringJson);
+
+    // visualizando o objeto retornado
     echo "<pre>";
-        var_dump(json_decode($json));
+        var_dump($objetoStringJson);
     echo "</pre>";
 
+    // exibindo conteudo da propriedade A do objeto
+    echo "Propriedade A do objeto: " . $objetoStringJson->a;
+
+    // transforma a string json em um array devido ter o segundo parametro como true
+    $arrayStringJson = json_decode($stringJson, true);
+
+    // visualizando o array retornado
     echo "<pre>";
-        var_dump(json_decode($json, true));
+        var_dump($arrayStringJson);
     echo "</pre>";
+
+    // exibindo conteudo da propriedade A do array
+    echo "Propriedade A do array: " . $arrayStringJson["a"];
 ?>
